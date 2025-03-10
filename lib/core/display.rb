0b2707d8b973a19ae2a4d2d8069ff7c0d4fa4e8f
dan <dis.131998@gmail.com>
2025-03-10 10:01:30 +0200

    Add Display class for rendering the chessboard

---
+++ lib/core/display.rb
@@ -0,0 +1,32 @@
+class Display
+  def initialize(board)
+    @board = board
+  end
+
+  def render
+    system("clear") || system("cls")
+    puts "\n"
+    print_headers
+    print_board
+    print_headers
+    puts "\n"
+  end
+
+  def print_headers
+    puts "\t\s\s\s\s\sa | b | c | d | e | f | g | h "
+  end
+
+ def print_board    
+    puts "\t\s\s\s---------------------------------"
+  
+    @board.grid.reverse.each_with_index do |row, i|
+      print_row = row.map do |piece|
+        print_piece = piece.nil? ? ' ' : piece.symbol
+      end
+
+      puts "\t" + "#{8 - i}\s\s" + "| #{print_row.join(' | ')} |" + "\s\s#{8 - i}"
+      puts "\t\s\s\s---------------------------------"
+    end
+  end
+
+end
