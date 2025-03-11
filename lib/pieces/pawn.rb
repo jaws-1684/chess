e92aa95cff180e165c135329d6faa098cc113dc4
dan <dis.131998@gmail.com>
2025-03-11 10:49:00 +0200

    feat: Add Pawn movement functions (double_step_forward, move)

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,7 +1,31 @@
 class Pawn < Piece
+  include Stepping
+
+  def initialize
+    super
+    @first_move =  true
+  end
+
+  def move(spos, tpos, board)
+    return true if step_forward(spos, tpos, board)
+
+    if @first_move
+      double_step_forward(spos, tpos, board)
+      
+      @first_move = false
+      return true
+    end
+
+    nil
+  end
+
   private
   
   def assign_symbol
     color == :white ? "♙" : "♟"
+  end
+
+  def double_step_forward(spos, tpos, board)
+    step_forward(spos, tpos, board, 2)
   end
 end
